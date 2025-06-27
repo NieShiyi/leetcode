137900936c0bffe277a9e0feeae6e0bd5927a23f
Nie Shiyi <shiyi.nie@qitantech.com>
2025-06-27 18:27:27 +0800

    fix: 修改launch.json方便debug

---
+++ 35.搜索插入位置.js
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=javascript
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode.cn/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (46.21%)
+ * Likes:    2520
+ * Dislikes: 0
+ * Total Accepted:    1.8M
+ * Total Submissions: 3.7M
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: nums = [1,3,5,6], target = 5
+ * 输出: 2
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: nums = [1,3,5,6], target = 2
+ * 输出: 1
+ *
+ *
+ * 示例 3:
+ *
+ *
+ * 输入: nums = [1,3,5,6], target = 7
+ * 输出: 4
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums 为 无重复元素 的 升序 排列数组
+ * -10^4 <= target <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+// TODO
+var searchInsert = function (nums, target) {
+  const n = nums.length;
+    let left = 0, right = n - 1, ans = n;
+    while (left <= right) {
+        let mid = ((right - left) >> 1) + left;
+        if (target <= nums[mid]) {
+            ans = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return ans;
+};
+// @lc code=end
